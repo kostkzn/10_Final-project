66aec008c24d8914100454a7620587588cde1e91
Kostiantyn Kuzin <audessie+github@gmail.com>
2023-02-28 00:47:15 +0200

    fixed playbooks in ansible and terraform

--- tf_ansible/01_hosts.sh
+++ tf_ansible/01_hosts.sh
@@ -4,7 +4,6 @@ mkdir /home/ubuntu/ansible
 cat <<- _EOF_ > /home/ubuntu/ansible/ansible.cfg
 [defaults]
 inventory = hosts
-remote_user = ubuntu
 host_key_checking = False
 timeout = 30
 gather_timeout = 30
@@ -14,19 +13,24 @@ _EOF_
 
 cat << _EOF_ > /home/ubuntu/ansible/hosts
 [ansible_control]
-control ansible_host=${node_1_ip} ansible_private_key_file=/home/ubuntu/.ssh/ci-swapp.pem
+control ansible_host=${node_1_ip}
 
 [webservers] 
-tomcat ansible_host=${node_2_ip} ansible_private_key_file=/home/ubuntu/.ssh/ci-swapp.pem
+tomcat ansible_host=${node_2_ip}
 
 [agents]
-agent ansible_host=${node_3_ip} ansible_private_key_file=/home/ubuntu/.ssh/ci-swapp.pem
+agent ansible_host=${node_3_ip}
 
 [tools]
-nexus ansible_host=${node_4_ip} ansible_private_key_file=/home/ubuntu/.ssh/ci-swapp.pem
-jenkins ansible_host=${node_5_ip} ansible_private_key_file=/home/ubuntu/.ssh/ci-swapp.pem
+nexus ansible_host=${node_4_ip}
+jenkins ansible_host=${node_5_ip}
 
 [docker:children] 
 tools
 agents
+
+[all:vars]
+ansible_user=ubuntu
+ansible_private_key_file=/home/ubuntu/.ssh/ci-swapp.pem
+
 _EOF_
--- tf_ansible/10_start.sh
+++ tf_ansible/10_start.sh
@@ -4,6 +4,6 @@ sudo chmod -v 400 /home/ubuntu/.ssh/ci-swapp*.pem
 sudo chown -v ubuntu:ubuntu /home/ubuntu/.ssh/ci-swapp*.pem
 
 sudo -u ubuntu bash -c 'cd /home/ubuntu/ansible; \
-ansible-playbook init-all.yml -i hosts -vvv | sudo tee -a /home/ubuntu/log_ansible'
+ansible-playbook init-all.yml -i hosts | sudo tee -a /home/ubuntu/log_ansible'
 
 echo ">>> STEP 5 Succeeded at $(date -u) -- Ansible stack configure"
